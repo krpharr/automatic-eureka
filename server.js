51af165a1277d5a5c4f05b983415378339ccd57d
rpharr <rpharr@richmond.edu>
2020-03-10 19:22:41 -0400

    added compression

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(compression());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
